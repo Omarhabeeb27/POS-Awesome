51a2a81b8bbb3ecfefff0661b932f6d0f5d6b08c
MohseenHabib <raonerama8@gmail.com>
2023-02-28 18:18:17 +0300

    commit

--- posawesome/public/js/posapp/components/Navbar.vue
+++ posawesome/public/js/posapp/components/Navbar.vue
@@ -6,7 +6,7 @@
         class="grey--text"
       ></v-app-bar-nav-icon>
       <v-img
-        src="/Users/mohseenhabib/POS-Awesome-1/posawesome/public/js/posapp/components/pos/sillage-ditribution-2.png"
+        src="https://saudisillage.com/wp-content/uploads/2021/03/sillage-ditribution.png"
         alt="POS Awesome"
         max-width="32"
         class="mr-2"
